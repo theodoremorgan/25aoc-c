5f475187dcd955c4287f18a6c06e782c01c2ae02
theo <theodore.joseph.morgan@gmail.com>
2026-01-08 23:04:27 +0000

    day 2 part 1

--- day-2/part1.c
+++ day-2/part1.c
@@ -1,4 +1,74 @@
-include <stddef.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char **argv){
+    printf("starting...\n");
+
+    printf("opening file...\n");
+    FILE *input = fopen("./data/input.txt", "r");
+
+    unsigned long lower;
+    unsigned long upper;
+
+    unsigned long sum;
+
+    //herein lies the problem
+    unsigned long tryroot;
+    unsigned long tryfull;
+
+    int32_t i;
+
+    int32_t dechalf;
+
+    while (fscanf(input, "%li-%li,", &lower, &upper) != EOF){
+
+        printf("lower is: %li. Upper is %li.\n", lower, upper);
+
+        int ndigits = log10(lower) + 1;
+
+        printf("digits: %i\n", ndigits);
+
+        dechalf = ndigits / 2;
+
+        if (ndigits % 2 != 0){
+        dechalf++;
+        }
+
+        tryroot = lower / pow(10, dechalf);
+
+        printf("tryroot: %li\n", tryroot);
+
+        int rootndigits = log10(tryroot) + 1;        
+        tryfull = tryroot * pow(10, rootndigits) + tryroot;
+
+        //printf("tryfull: %li\n", tryfull);
+
+        while (tryfull <= upper){
+            if (tryfull >= lower){
+                printf("tryfull: %li\n", tryfull);
+                sum += tryfull;
+            }
+            tryroot++;
+            rootndigits = log10(tryroot) + 1;        
+            tryfull = tryroot * pow(10, rootndigits) + tryroot;
+        }
+        printf("\n");
+    }
+
+
+    printf("Final score: %li.\n", sum);
+    printf("closing file & finishing up \n");
+    fclose(input);
+
+    return 0;
+}
+
+/*
+
+if length is odd;
+    start first try from 
+    len + 1/2
+*/
